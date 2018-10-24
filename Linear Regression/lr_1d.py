483510e57c0f93ee67bf3ac28ee7bf7924513a59
hrajgarhia <hrajgarhia943@gmail.com>
2018-10-24 11:32:05 +0530

    set to start

---
+++ Linear Regression/lr_1d.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+path='/Users/harshitrajgarhia/PycharmProjects/Machine-Learning/machine_learning_examples-master/linear_regression_class/'
+
+a=pd.read_csv(path+'data_1d.csv')
+
