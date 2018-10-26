d8245547f61b2cf349eb7a8f4408af19a9d62a0b
hrajgarhia <hrajgarhia943@gmail.com>
2018-10-26 09:32:49 +0530

    code for linear regression with one independent variable

--- Linear Regression/lr_1d.py
+++ Linear Regression/lr_1d.py
@@ -4,5 +4,44 @@ import matplotlib.pyplot as plt
 
 path='/Users/harshitrajgarhia/PycharmProjects/Machine-Learning/machine_learning_examples-master/linear_regression_class/'
 
-a=pd.read_csv(path+'data_1d.csv')
+# load the data
+X=[]
+Y=[]
 
+for line in open(path+'data_1d.csv'):
+    x,y = line.split(',')
+    X.append(float(x))
+    Y.append(float(y))
+
+# converting lists to numpy array since they are more useful
+
+X=np.array(X)
+Y=np.array(Y)
+
+#PLOT the data
+
+plt.scatter(X,Y)
+plt.show()
+
+#calculate a and b from the formula derived
+
+denominator= X.dot(X)-X.mean()*X.sum()
+a= (X.dot(Y) -X.mean()*Y.sum())/denominator
+b=(Y.mean()*(X.dot(X))-X.mean()*X.dot(Y))/denominator
+
+yhat = a*X + b
+
+#plot the line of best fit
+
+plt.scatter(X,Y)
+plt.plot(X,yhat)
+plt.show()
+
+#calculate r-squared error
+
+d1=Y-yhat
+d2=Y-Y.mean()
+
+rsq= 1-(d1.dot(d1))/(d2.dot(d2))
+
+print("R-squared is :", rsq)
